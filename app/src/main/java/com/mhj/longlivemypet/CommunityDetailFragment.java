768b561c108a7b2c555480cbfa5d2edb434f22bf
koreadydtkd <dydtkddl2@nate.com>
2020-02-10 12:41:39 +0900

    임ㄱ시

--- app/src/main/java/com/mhj/longlivemypet/CommunityDetailFragment.java
+++ app/src/main/java/com/mhj/longlivemypet/CommunityDetailFragment.java
@@ -8,11 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DetailFragment extends Fragment {
+public class CommunityDetailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_community_detail, container, false);
+        return rootView;
     }
 
 }
