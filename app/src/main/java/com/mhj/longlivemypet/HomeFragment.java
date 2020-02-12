3802b282f2f0a3b4c55be838eebd9c142ca34be6
koreadydtkd <dydtkddl2@nate.com>
2020-02-12 14:29:36 +0900

    2020-02-12 용상

--- app/src/main/java/com/mhj/longlivemypet/HomeFragment.java
+++ app/src/main/java/com/mhj/longlivemypet/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.mhj.longlivemypet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +8,26 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeFragment extends Fragment {
-    private static final String TAG = "HomeFragment";
+    private FirebaseAuth auth;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+        auth = FirebaseAuth.getInstance();
+
+        //로그아웃 임시
+        rootView.findViewById(R.id.button_logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.finish();
+                startActivity(new Intent(getContext(), LoginActivity.class));
+            }
+        });
 
         return rootView;
     }
